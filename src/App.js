a56b6a5ea1c9fa64a77fb597c84975495bee806a
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-03-18 11:46:50 -0400

    React basics & JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const title = 'Blog post';
+    const body = 'This is my blog post';
+
+    const comments = [
+        {id: 1, text: 'Comment one'},
+        {id: 2, text: 'Comment two'},
+        {id: 3, text: 'Comment three'}
+    ];
+
+    const loading = false;
+    const showComments = true;
+
+    if(loading) return <h1>Loading...</h1>
+
+    return (
+        <div className="container">
+            <h1>{ title.toUpperCase() }</h1>
+            <p>{ body }</p>
+
+            {showComments && (
+                <div className="comments">
+                    <h3>Comments ({comments.length})</h3>
+
+                    <ul>
+                        {comments.map( comment => {
+                            return <li key={ comment.id }>{comment.text}</li>
+                        })}
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
 }
+
 
 export default App;

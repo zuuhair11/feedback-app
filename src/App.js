6855da85da8c48fcf5ef8603cf284bcecc09f21a
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-03-19 18:49:34 -0400

    Components, Props & State

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
+import { useState } from 'react' ;
+import Header from './components/Header' ;
+import FeedbackList from './components/FeedbackList' ;
+import FeedbackStats from './components/FeedbackStats' ;
+import FeedbackData from './data/feedbackData' ;
+
+
 function App() {
-    const title = 'Blog post';
-    const body = 'This is my blog post';
+    const [feedback, setFeedback] = useState(FeedbackData);
 
-    const comments = [
-        {id: 1, text: 'Comment one'},
-        {id: 2, text: 'Comment two'},
-        {id: 3, text: 'Comment three'}
-    ];
-
-    const loading = false;
-    const showComments = true;
-
-    if(loading) return <h1>Loading...</h1>
-
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete?')) {
+            setFeedback( prevFeedback => {
+                return prevFeedback.filter( item => item.id !== id);
+            });
+        }
+    }
+    
     return (
-        <div className="container">
-            <h1>{ title.toUpperCase() }</h1>
-            <p>{ body }</p>
-
-            {showComments && (
-                <div className="comments">
-                    <h3>Comments ({comments.length})</h3>
-
-                    <ul>
-                        {comments.map( comment => {
-                            return <li key={ comment.id }>{comment.text}</li>
-                        })}
-                    </ul>
-                </div>
-            )}
-        </div>
+        <>
+            <Header />
+            <div className="container">
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                    feedback={ feedback }
+                    handleDelete={ deleteFeedback }
+                />
+            </div>
+        </>
     );
 }
 
---
+++ src/components/FeedbackItem.jsx
@@ -0,0 +1,29 @@
+import { FaTimes } from 'react-icons/fa' ;
+import PropTypes from 'prop-types' ;
+import Card from "./shared/Card" ;
+
+
+function FeedbackItem({ item, handleDelete }) {
+    
+    return (
+        <Card>
+            <div className="num-display">{ item.rating }</div>
+            <button 
+                className='close'
+                onClick={ () => handleDelete(item.id) }
+            >
+                <FaTimes color='purple' />
+            </button>
+            <p className="text-display">{ item.text }</p>
+        </Card>
+    );
+}
+
+
+
+FeedbackItem.propTypes = {
+    item: PropTypes.object.isRequired
+}
+
+
+export default FeedbackItem;

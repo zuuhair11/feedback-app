006b1f47ba06137454ecf47e90d9938279ec0acb
Zouhair Sahtout <zouhairsahtout66@gmail.com>
2023-03-24 19:07:02 -0400

    Forms, Validation & Simple Animation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react' ;
 import Header from './components/Header' ;
 import FeedbackList from './components/FeedbackList' ;
 import FeedbackStats from './components/FeedbackStats' ;
+import FeedbackForm from './components/FeedbackForm' ;
 import FeedbackData from './data/feedbackData' ;
 
 
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
+    const addFeedback = (newFeedback) => {
+        setFeedback( prevFeedback => {
+            newFeedback.id = uuidv4();
+            return [newFeedback, ...prevFeedback]
+        });
+    }
+    
     const deleteFeedback = (id) => {
         if(window.confirm('Are you sure you want to delete?')) {
             setFeedback( prevFeedback => {
@@ -20,6 +29,7 @@ function App() {
         <>
             <Header />
             <div className="container">
+                <FeedbackForm handleAdd={ addFeedback } />
                 <FeedbackStats feedback={feedback} />
                 <FeedbackList
                     feedback={ feedback }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import PropTypes from 'prop-types' ;
 import FeedbackItem from "./FeedbackItem" ;
 
@@ -7,17 +8,39 @@ function FeedbackList({ feedback, handleDelete }) {
         return (<p>No Feedback Yet</p>);
     }
 
+    // Using frame motion
     return (
         <div className="feedback-list">
-            {feedback.map( item => (
-                <FeedbackItem
-                    key={ item.id }
-                    item={ item }
-                    handleDelete={ handleDelete }
-                />
-            ))}
+            <AnimatePresence >
+                {feedback.map( item => (
+                    <motion.div 
+                        key={ item.id }
+                        initial={ {opacity: 0} }
+                        animate={ {opacity: 1} }
+                        exit={ {x: "calc(100vw )", opacity: 0 } }
+                    >
+                        <FeedbackItem
+                            key={ item.id }
+                            item={ item }
+                            handleDelete={ handleDelete }
+                        />
+                    </motion.div>
+                ))}
+            </AnimatePresence>
         </div>
     );
+
+    // return (
+    //     <div className="feedback-list">
+    //         {feedback.map( item => (
+    //             <FeedbackItem
+    //                 key={ item.id }
+    //                 item={ item }
+    //                 handleDelete={ handleDelete }
+    //             />
+    //         ))}
+    //     </div>
+    // );
 }
 
 
@@ -26,7 +49,7 @@ FeedbackList.propTypes = {
     // feedback: PropTypes.array
     feedback: PropTypes.arrayOf(
         PropTypes.shape({
-            id: PropTypes.number.isRequired,
+            id: PropTypes.string.isRequired,
             text: PropTypes.string.isRequired,
             rating: PropTypes.number.isRequired
         }),
